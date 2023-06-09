2a7268dbc8295e1fc127589282df86d8e02edb12
Bui Phuong Nam <e0851451@u.nus.edu>
2023-06-09 17:28:46 +0800

    Add Choice to add text headings

--- VerticalWriting/script.js
+++ VerticalWriting/script.js
@@ -1,26 +1,30 @@
 function transformText() {
-    var input = document.getElementById("input").value;
-    var sentences = input.split(/[.\n?!]+/);
+    const inputText = document.getElementById("input").value;
+    const textName = document.getElementById("text-name").value;
+    const sentences = inputText.split(/[.\n?!]+/);
 
-    var output = document.getElementById("output");
+    const output = document.getElementById("output");
     output.innerHTML = "";
 
-    var previousSentenceBoxCount = 0; // Number of boxes in the previous sentence
+    let previousSentenceBoxCount = 0; // Number of boxes in the previous sentence
 
-    for (var i = sentences.length - 1; i >= 0; i--) {
-        var sentence = sentences[i].replace(/[.,?!“”‘’]/g, "").trim();
-        console.log(sentence);
+    for (let i = 0; i < sentences.length; i++) {
+        const sentence = sentences[i].replace(/[.,?!“”‘’]/g, "").trim();
+
         if (sentence !== "") {
-            var column = document.createElement("div");
+            const column = document.createElement("div");
             column.classList.add("column");
 
-            var sentenceBoxCount = 0; // Number of boxes in the current sentence
+            let sentenceBoxCount = 0; // Number of boxes in the current sentence
 
-            Array.from(sentence).forEach(function(character) {
+            Array.from(sentence).forEach(function (character) {
                 if (character !== " ") {
-                    var box = document.createElement("div");
+                    const box = document.createElement("div");
                     box.classList.add("box");
                     box.innerText = character;
+                    if (isLatinCharacter(character)) {
+                        box.classList.add("latin");
+                    }
                     column.appendChild(box);
                     sentenceBoxCount++;
                 }
@@ -28,7 +32,7 @@ function transformText() {
 
             if (sentenceBoxCount === 0) {
                 // Empty sentence, create an empty box with width equal to normal box and height calculated based on previous sentence
-                var emptyBox = document.createElement("div");
+                const emptyBox = document.createElement("div");
                 emptyBox.classList.add("box", "empty");
                 emptyBox.style.width = column.firstChild.offsetWidth + "px";
                 emptyBox.style.height = previousSentenceBoxCount * 25 + "px";
@@ -39,6 +43,28 @@ function transformText() {
             previousSentenceBoxCount = sentenceBoxCount;
         }
     }
+
+    if (textName) {
+        const textNameElement = document.createElement("div");
+        textNameElement.classList.add("text-name");
+
+        Array.from(textName).forEach(function (character) {
+            const characterElement = document.createElement("div");
+            characterElement.innerText = character;
+            if (isLatinCharacter(character)) {
+                characterElement.classList.add("latin");
+            }
+            textNameElement.appendChild(characterElement);
+        });
+
+        const textNameWrapper = document.createElement("div");
+        textNameWrapper.appendChild(textNameElement);
+        output.insertBefore(textNameWrapper, output.firstChild); // Insert text name at the top
+    }
 }
 
+function isLatinCharacter(character) {
+    const latinAlphabetRegex = /^[A-Za-z]+$/;
+    return latinAlphabetRegex.test(character);
+}
 
